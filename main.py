be66fd8fbf822a2e66903e21885af342c4ae21b4
spitamo <spitamo@hotmail.com>
2026-07-12 15:38:57 -0700

    test: translation callback

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-from infrastructure.hotkeys import HotkeyListener
+from infrastructure.hotkeys import HotkeyListener as h
+from infrastructure.translate.service import TranslatorService as t
+import time
+
+translator = t(.3) 
+
+hotkey = h("ctrl+c", 0.30, lambda word : print(f"word : {word} -> {translator.translate(word, 'en', 'fa')}"))
+
+hotkey.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    hotkey.stop()
+
