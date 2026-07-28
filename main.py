e6906f5ba64abbc3f91b37ee731286d253cd54c6
spitamo <spitamo@hotmail.com>
2026-07-28 14:23:31 -0700

    fix: catch KeyboardInterrupt

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from config import Config
 from pipeline.app import run_app
 
 print("main.py started")
-
 def main():
-    cfg = Config()
-    asyncio.run(run_app(cfg))
-
+    try:
+        cfg = Config()
+        asyncio.run(run_app(cfg))
+    except KeyboardInterrupt:
+        print("Stopping...")
 if __name__ == "__main__":
     main()
