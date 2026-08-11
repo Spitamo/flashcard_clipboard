fed48ab4762426196523e2ea59f7195a026af247
soroosh <sorooshdpki@gmail.com>
2026-08-11 11:41:02 +0330

    FIX: remove redundant db_worker

--- pipeline/app.py
+++ pipeline/app.py
@@ -43,7 +43,7 @@ async def run_app(cfg: Config):
    uv_config = uvicorn.Config(
         "api:app",
         host="127.0.0.1",
-        port=8000,
+        port=cfg.api_port,
         log_level="warning",
     )
 
@@ -67,14 +67,6 @@ async def run_app(cfg: Config):
             notify_fn=notify_new_card,    
          ),
          name="db_worker",
-      ),
-      asyncio.create_task(
-         db_worker(
-            queues.translated_queue,
-            repo,
-            notify_fn=notify_new_card,    
-         ),
-         name="db_worker",
         ),
         asyncio.create_task(
             uv_server.serve(),                
@@ -83,7 +75,7 @@ async def run_app(cfg: Config):
       
    ]
 
-   print("Running on http://127.0.0.1:8000")
+   print("Running on http://127.0.0.1:51847")
    print("Running. Select a word and press Ctrl+C (hotkey).")
    print("To stop the app: focus terminal and press Ctrl+Break or Ctrl+C (terminal interrupt).")
 
