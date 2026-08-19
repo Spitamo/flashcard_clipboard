5b410825c8736190bcdee3aab2c97d02b01df476
spitamo <spitamo@hotmail.com>
2026-08-19 14:23:44 -0700

    Merge selected changes from #4

--- tauri-ui/src-tauri/src/lib.rs
+++ tauri-ui/src-tauri/src/lib.rs
@@ -1,4 +1,6 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri_plugin_shell::ShellExt;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -8,6 +10,19 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            let sidecar = app
+                .shell()
+                .sidecar("flashcard-backend")
+                .unwrap();
+
+            sidecar
+                .spawn()
+                .expect("failed to start backend");
+
+            Ok(())
+        }) 
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
